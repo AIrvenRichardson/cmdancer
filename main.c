68662abfc9956deb959f73c4b4d5e6a311eafb7b
Adam <goober@goobertop.goobertop.com>
2025-02-27 16:59:16 -0700

    Kind of works, probably need to steal more tsoding ideas to make it not look ass

--- main.c
+++ main.c
@@ -2,16 +2,35 @@
 #include <ncurses.h>
 #include <stdint.h>
 #include <string.h>
+#include <limits.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #define MAX_NAME_SIZE 30
 
 void ProcessAudio(void *buffer, unsigned int frames)
 {
-    uint32_t *samples = (uint32_t *)buffer;
+    clear();
+    int16_t *samples = (int16_t *)buffer;
+    int fpc = (int)frames / COLS;
 
-    printf("%d ", *samples);
-    printf("\n");
+    for (unsigned int frame = 0; frame < frames; frame+=fpc)
+    {
+        int16_t *l = &samples[frame * 2 + 0], *r = &samples[frame * 2 + 1];
+        int16_t comp = *l;
+        float scaled = (float)comp/(float)INT16_MAX;
+        comp = (int16_t)(scaled * ((float)(LINES-2)/2));
+
+        int start = 0;
+        while (start != comp)
+        {
+            mvaddch((LINES/2)+start,frame/fpc, '|');
+            if (start > comp) start--;
+            else start++;
+        }
+        
+    }
+    refresh();
 }
 
 
@@ -44,6 +63,7 @@ int main(int argc, char *argv[])
     Music music = LoadMusicStream(resourcepath);
     PlayMusicStream(music);
     float timePlayed = 0.0f;
+    bool paused = false;
 
     while (true)
     {
@@ -61,10 +81,17 @@ int main(int argc, char *argv[])
         {
             break;
         }
+        else if (input == 'p')
+        {
+            paused = !paused;
+            if (paused) PauseMusicStream(music);
+            else ResumeMusicStream(music);
+        }
         
         timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
         if (timePlayed > 1.0f) timePlayed = 1.0f;
-        refresh();
+        //clear();
+        //refresh();
     }
     //De-init everything
     endwin();
